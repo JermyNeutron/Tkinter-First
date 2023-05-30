6c9675193d289cc8f0c42279e216ff508433ebd5
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2023-05-29 19:59:59 -0700

    added a premature button widget

---
+++ buttons.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# button widget
+
+root = Tk()
+
+# everything in tkinter is a widget, so a button is a widget
+
+# create a function for something to happen
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a button!!")
+    myLabel.pack()
+
+# myButton = Button(root, text="Click Me!", state=DISABLED, padx=50, pady=50)
+myButton = Button(root, text="Click Me!", command=myClick)
+myButton.pack()
+
+root.mainloop()
