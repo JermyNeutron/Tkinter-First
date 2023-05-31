5b091f2545503cb7d2515149dd0f508d15b68651
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2023-05-30 22:49:49 -0700

    added additional operators
    added multiplication, divistion, and subtraction. Rearranged buttons and adjusted sizing for consistency

--- basic calculator/calculator.py
+++ basic calculator/calculator.py
@@ -5,9 +5,9 @@ from tkinter import *
 root = Tk()
 root.title("Simple Calculator")
 
-e = Entry(root, width=35, borderwidth=5)
+e = Entry(root, width=48, borderwidth=5)
 # columnspan to span across how many columns
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
 # e.insert(0, "Enter your name")
 
 # add value function
@@ -32,15 +32,53 @@ def add():
     first_number = e.get()
     # set global variable
     global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
+    
+# adding subtract function
+def subtract():
+    first_number = e.get()
+    # set global variable
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+# adding divide function
+def divide():
+    first_number = e.get()
+    # set global variable
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+# adding multiply function
+def multiply():
+    first_number = e.get()
+    # set global variable
+    global f_num
+    global math
+    math = "multiplication"
     f_num = int(first_number)
     e.delete(0, END)
 
 # want to grab 
 def equal():
-    global s_num
     s_num = e.get()
     e.delete(0, END)
-    e.insert(0, f_num + int(s_num))
+    if math == "addition":
+        e.insert(0, f_num + int(s_num))
+    if math == "subtraction":
+        e.insert(0, f_num - int(s_num))
+    if math == "division":
+        e.insert(0, f_num / int(s_num))
+    if math == "multiplication":
+        e.insert(0, f_num * int(s_num))
 
 # define buttons
 
@@ -56,28 +94,35 @@ button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click
 button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_0 = Button(root, text="0", padx=87, pady=20, command=lambda: button_click(0))
 button_clear = Button(root, text="C", padx=39, pady=20, command=clear)
-button_addition = Button(root, text="+", padx=87, pady=20, command=add)
-button_equal = Button(root, text="=", padx=87, pady=20, command=equal)
+button_addition = Button(root, text="+", padx=39, pady=20, command=add)
+button_equal = Button(root, text="=", padx=40, pady=53, command=equal)
+button_subtract = Button(root, text="-", padx=40, pady=20, command=subtract)
+button_divide = Button(root, text="/", padx=40, pady=20, command=divide)
+button_multiply = Button(root, text="x", padx=40, pady=20, command=multiply)
 
 # place buttons on screen
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
 
-button_0.grid(row=4, column=0)
-button_clear.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
-button_addition.grid(row=4, column=1, columnspan=2)
+button_0.grid(row=5, column=0, columnspan=2)
+
+button_clear.grid(row=1, column=0)
+button_equal.grid(row=4, column=3, rowspan=2)
+button_addition.grid(row=3, column=3)
+button_subtract.grid(row=2, column=3)
+button_divide.grid(row=1, column=2)
+button_multiply.grid(row=1, column=3)
 
 myButton = Button(root, text="Enter your name")
 
